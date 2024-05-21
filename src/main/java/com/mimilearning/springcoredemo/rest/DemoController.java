f3a61016387d0739cb9f4552afdde2fffacc95ad
Mariem Moula <moula.meriame@gmail.com>
2024-05-21 11:35:00 +0100

    Initial commit

---
+++ src/main/java/com/mimilearning/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.mimilearning.springcoredemo.rest;
+
+import com.mimilearning.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach ;
+
+
+    //define a constructor for dependency injection
+    @Autowired //if you have one constructor it s optional
+    public DemoController(Coach theCoach){
+        myCoach=theCoach;
+    }
+
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
